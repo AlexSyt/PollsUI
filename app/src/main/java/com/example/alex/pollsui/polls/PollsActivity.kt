d233bf76d1060c5bcc99684cc4c6f6d51c9ffca8
Alex Sytnyk <asytnyk0@gmail.com>
2018-05-04 18:13:32 +0300

    Rename activity and related stuff

--- app/src/main/java/com/example/alex/pollsui/polls/PollsActivity.kt
+++ app/src/main/java/com/example/alex/pollsui/polls/PollsActivity.kt
@@ -1,13 +1,13 @@
-package com.example.alex.pollsui.mypolls
+package com.example.alex.pollsui.polls
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.example.alex.pollsui.R
 
-class MyPollsActivity : AppCompatActivity() {
+class PollsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_polls)
+        setContentView(R.layout.activity_polls)
     }
 }
