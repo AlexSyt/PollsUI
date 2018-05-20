ff1f19a4601ed5b407a4a54700d32f5b1e4ea959
Alex Sytnyk <asytnyk0@gmail.com>
2018-05-21 00:13:34 +0300

    Add base REST functionality

---
+++ app/src/main/java/com/example/alex/pollsui/data/source/remote/PollsApiService.kt
@@ -0,0 +1,46 @@
+package com.example.alex.pollsui.data.source.remote
+
+import android.util.Base64
+import com.example.alex.pollsui.data.Poll
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface PollsApiService {
+
+    @GET("getPollsList")
+    fun getPollsList(): Call<List<Poll>>
+
+    @GET("getMyPollsList")
+    fun getMyPollsList(): Call<List<Poll>>
+
+    @GET("getPoll")
+    fun getPoll(@Query("id") id: String): Call<Poll>
+
+    companion object Factory {
+
+        fun create(): PollsApiService {
+            val credentials = "user1:password"
+            val basic = "Basic " + Base64.encodeToString(credentials.toByteArray(), Base64.NO_WRAP)
+
+            val httpClient = OkHttpClient.Builder()
+                    .addInterceptor {
+                        it.proceed(it.request()
+                                .newBuilder()
+                                .addHeader("Authorization", basic)
+                                .build())
+                    }
+                    .build()
+
+            return Retrofit.Builder()
+                    .baseUrl("https://warm-retreat-17416.herokuapp.com/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(httpClient)
+                    .build()
+                    .create(PollsApiService::class.java)
+        }
+    }
+}
--- app/src/main/java/com/example/alex/pollsui/data/source/remote/PollsRemoteDataSource.kt
+++ app/src/main/java/com/example/alex/pollsui/data/source/remote/PollsRemoteDataSource.kt
@@ -1,25 +1,54 @@
 package com.example.alex.pollsui.data.source.remote
 
-import android.os.Handler
 import com.example.alex.pollsui.data.Poll
 import com.example.alex.pollsui.data.source.PollsDataSource
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class PollsRemoteDataSource : PollsDataSource {
 
-    private val DELAY = 2500L
+    private val apiService = PollsApiService.create()
 
     override fun getPolls(authorId: String?, callback: PollsDataSource.LoadPollsCallback) {
-        // FIXME
-        Handler().postDelayed({
-            callback.onDataNotAvailable()
-        }, DELAY)
+        getPollsCall(authorId).enqueue(object : Callback<List<Poll>> {
+            override fun onResponse(call: Call<List<Poll>>?, response: Response<List<Poll>>?) {
+                val polls = response?.body()
+                if (polls != null) {
+                    callback.onPollsLoaded(polls)
+                } else {
+                    callback.onDataNotAvailable()
+                }
+            }
+
+            override fun onFailure(call: Call<List<Poll>>?, t: Throwable?) {
+                callback.onDataNotAvailable()
+            }
+        })
+    }
+
+    private fun getPollsCall(authorId: String?): Call<List<Poll>> {
+        if (authorId != null) {
+            return apiService.getMyPollsList()
+        }
+        return apiService.getPollsList()
     }
 
     override fun getPoll(pollId: String, callback: PollsDataSource.GetPollCallback) {
-        // FIXME
-        Handler().postDelayed({
-            callback.onDataNotAvailable()
-        }, DELAY)
+        apiService.getPoll(pollId).enqueue(object : Callback<Poll> {
+            override fun onResponse(call: Call<Poll>?, response: Response<Poll>?) {
+                val poll = response?.body()
+                if (poll != null) {
+                    callback.onPollLoaded(poll)
+                } else {
+                    callback.onDataNotAvailable()
+                }
+            }
+
+            override fun onFailure(call: Call<Poll>?, t: Throwable?) {
+                callback.onDataNotAvailable()
+            }
+        })
     }
 
     override fun createPoll(poll: Poll) {
