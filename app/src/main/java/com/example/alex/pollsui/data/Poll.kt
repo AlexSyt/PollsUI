ff9e90903f627557cc6f411c5939a94a8015e9ed
Alex Sytnyk <asytnyk0@gmail.com>
2018-05-22 23:57:04 +0300

    Remove redundant field

--- app/src/main/java/com/example/alex/pollsui/data/Poll.kt
+++ app/src/main/java/com/example/alex/pollsui/data/Poll.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName
 
 data class Poll constructor(
         @SerializedName("name") val title: String,
-        @SerializedName("author") val author: String,
+        @SerializedName("author") val author: String? = null,
         @SerializedName("id") val id: String? = null,
         @SerializedName("questions") val questions: MutableList<Question> = ArrayList()
 )
